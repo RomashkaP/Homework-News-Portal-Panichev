dfe09259df2c5e5691d8e628b8a15738d1a6a3f9
Roman <panichevr@gmail.com>
2025-07-14 15:15:45 +0300

    Добавил домашнюю работу из 18 модуля.

---
+++ portal/portal/templatetags/custom_filters.py
@@ -0,0 +1,19 @@
+from django import template
+#from portal.resources import CENSOR_LIST
+register = template.Library()
+
+CENSOR_LIST = ['достиг', 'связывают', 'Солнце', 'аллеи', 'домов', 'first', 'парка', 'жилья', 'сообщили', 'планеты']
+@register.filter()
+def censor(value):
+    if not isinstance(value, str):
+        raise TypeError('Ожидается строка.')
+    else:
+        words = value.split()
+        censored_words = []
+        for word in words:
+            if word in CENSOR_LIST:
+                censored_word = f'{word[0]}***'
+                censored_words.append(censored_word)
+            else:
+                censored_words.append(word)
+    return ' '.join(censored_words)
--- portal/portal/views.py
+++ portal/portal/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+# from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
-    ordering = 'time_in'
+    ordering = '-time_in'
     template_name = 'postlist.html'
     context_object_name = 'postlist'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+
 
 class PostDetail(DetailView):
     model = Post
